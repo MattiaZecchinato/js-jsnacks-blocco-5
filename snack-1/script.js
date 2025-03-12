66b046b81252ebdebf014b75b908b03b86b3fcad
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-12 14:55:48 +0100

    feat: add bonus snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -7,3 +7,12 @@ names.forEach(element => {
 
     console.log(element);
 });
+
+// BONUS
+console.log('---');
+
+for (let i = 0; i < names.length; i++) {
+
+    const element = names[i];
+    console.log(element);
+}
